da278b4affacd4b01d8692c44ba157c4dc6a0d2b
Vaishnavi Yadav <vaishu.yadav2410@gmail.com>
2026-07-30 15:37:29 +0530

    Accounts module progress

--- milestone1/backend/accounts/views.py
+++ milestone1/backend/accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'accounts/home.html')
+
+def login_view(request):
+    return render(request, 'accounts/login.html')
+
+def register_view(request):
+    return render(request, 'accounts/register.html')
