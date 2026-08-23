9bd22590de77ce85f01cc383c6c941f774005e06
Vaishnavi Yadav <vaishu.yadav2410@gmail.com>
2026-08-23 11:02:40 +0530

    updated dashboard colors

--- milestone1/backend/backend/settings.py
+++ milestone1/backend/backend/settings.py
@@ -133,13 +133,5 @@ LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/recruiter/dashboard/"
 LOGOUT_REDIRECT_URL = "/login/"
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = "yourgmail@gmail.com"
-EMAIL_HOST_PASSWORD = "your-16-character-app-password"
-
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
